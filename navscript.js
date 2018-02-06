c51dfd7812d05c1c1ca07b18b840a2544ba65893
Brunell <E080099@TMH.ORG>
2018-02-06 08:30:27 -0500

    foobar

---
+++ navscript.js
@@ -0,0 +1,9 @@
+document.getElementById("mainnav").innerHTML =
+'<ul>'+
+ '<li id="ahome"><a href="index.html">Home</a></li>'+
+ '<li id="aserver"><a href="servers.html">Server Info</a></li>'+
+ '<li id="adocs"><a href="documentation.html">Documentation</a></li>'+
+ '<li id="adocs"><a href="troubleshoot.html">Troubleshooting</a></li>'+
+ '<li id="ainterface"><a href="systems.html">Interfaced Systems</a></li>'+
+ '<li id="alinks"><a href="links.html">Links</a></li>'+
+'</ul>';
