e841da591600ae24d710d2e1ccd10b9dcee16367
U-MAIN\e080099 <e080099@MJ15XNP.MAIN.AD.TMH.ORG>
2018-07-13 11:31:34 -0400

    foobar

--- navscript.js
+++ navscript.js
@@ -1,10 +1,12 @@
 document.getElementById("mainnav").innerHTML =
 '<ul>'+
  '<li id="ahome"><a href="index.html">Home</a></li>'+
- '<li id="alinks"><a href="mirth.html">Mirth</a></li>'+
- '<li id="alinks"><a href="cerner.html">Cerner</a></li>'+
- '<li id="alinks"><a href="links.html">Links</a></li>'+
- '<li id="ainterface"><a href="systems.html">Systems</a></li>'+
+ '<li id="alinks"><a href="hl7.html">HL7 Messages</a></li>'+
+ '<li id="alinks"><a href="mirth.html">Mirth Tutorials</a></li>'+
+ '<li id="alinks"><a href="cerner.html">Cerner Tutorials</a></li>'+
+ '<li id="adocs"><a href="cernerdocs.html">Cerner Documentation</a></li>'+
+ '<li id="adocs"><a href="troubleshoot.html">Cerner Troubleshooting</a></li>'+
+ '<li id="ainterface"><a href="systems.html">Integrated Systems</a></li>'+
  '<li id="alinks"><a href="powershell.html">Powershell</a></li>'+
- '<li id="adocs"><a href="troubleshoot.html">Troubleshooting</a></li>'+
+ '<li id="alinks"><a href="links.html">Links</a></li>'+
 '</ul>'
